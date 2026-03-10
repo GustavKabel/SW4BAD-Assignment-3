b37eb27eae08d8ebdafd75d0034364b141cfa360
gusta <au729039@uni.au.dk>
2026-03-10 14:32:31 +0100

    more build stuff

--- obj/Debug/net10.0/AarhusSpaceProgram.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/AarhusSpaceProgram.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AarhusSpaceProgram.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fd8b5dc3a570a954ea29850d07537ac015bff47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4d46482c3ebbc44345656ea402f8c311b6c521a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AarhusSpaceProgram.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AarhusSpaceProgram.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
